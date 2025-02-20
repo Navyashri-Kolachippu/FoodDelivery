2e67286dedaa6d88c782269f0c5125b92e858e2d
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-02-20 18:24:18 +0530

    shimmer changes and api call

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,21 @@
 import {RestaurantCard } from "../components/RestaurantCard";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { resList  } from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () =>{
-    const [listOfResturants,setListOfRestaurants]= useState(resList);
-    return (
+    const [listOfResturants,setListOfRestaurants]= useState([]);
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    const fetchData= async()=>{
+        const data= await fetch("http://localhost:5220/FoodDelivery");
+        const json = await data.json();
+        console.log(json);
+        setListOfRestaurants(json);
+    };
+
+    return (listOfResturants.length == 0)?  <Shimmer /> : (
         <div className="body">
             <div className="filter"> 
                 <button className="filter-btn"
