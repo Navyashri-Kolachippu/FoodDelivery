4466124ddfd820ade647f7d72658691e0b974a70
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-03-15 01:35:54 +0530

    menu tailwind

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -14,18 +14,18 @@ const RestaurantMenu = ()=>{
  {
     const {restaurantName,cuisines=[],costForTwo,items=[],restaurantId}=menudata;
     return(
-        <div className="menu">
-            <h1>{restaurantName}</h1>
+        <div className="p-4">
+            <h1 className="text-xl font-bold">{restaurantName}</h1>
             <h3>{cuisines.join(", ")} - Rs.{costForTwo}</h3>
-            <h3>Menu</h3>
-            <ul> 
+            <h3 className="mt-4 text-lg font-semibold">Menu</h3>
+            <ul className="space-y-4 bg-gray-100 shadow-md"> 
                 {items.map((item)=> (
-               <li key={item.dishName} className="menu-item">
-               <div className="menu-details">
-                   <h4 className="dish-name">{item.dishName}</h4>
-                   <p className="dish-price">Rs. {item.price}</p>
+               <li key={item.dishName} className="flex items-center gap-4 border p-4 rounded-lg shadow-md">
+               <div className="flex-1">
+                   <h4 className="text-lg font-semibold">{item.dishName}</h4>
+                   <p className="text-gray-600">Rs. {item.price}</p>
                </div>
-               <img className="dish-img" alt={item.dishName} src={item.dishImage} />
+               <img className="w-32 h-32 object-cover rounded-lg" alt={item.dishName} src={item.dishImage} />
                </li>
                 ))}
             </ul>
