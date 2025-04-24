58cfeba3e30acc73d1a0ca733dc9bc24a45cc91f
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-04-24 21:08:54 +0530

    formatting

--- src/components/__tests__/Cart.test.js
+++ src/components/__tests__/Cart.test.js
@@ -27,23 +27,27 @@ it("Should load the restaurnt menu list",async()=>{
                 <Cart />
             </Provider>
         </BrowserRouter>));
+    
     const accordionHeader = screen.getByText("Baked Items(2)");
     fireEvent.click(accordionHeader);
 
     const items=screen.getAllByTestId("menuItem");
     expect(items.length).toBe(2);
-
     expect(screen.getByText("Cart - (0 -Items)")).toBeInTheDocument();
+
     const buttons = screen.getAllByRole("button",{name:"Add +"});
     fireEvent.click(buttons[0]);
+
     expect(screen.getByText("Cart - (1 -Items)")).toBeInTheDocument();
+
     fireEvent.click(buttons[1]);
+    
     expect(screen.getByText("Cart - (2 -Items)")).toBeInTheDocument();
     expect(screen.getAllByTestId("menuItem").length).toBe(4);
 
     fireEvent.click(screen.getByRole("button",{name:"Clear cart"}));
+    
     expect(screen.getAllByTestId("menuItem").length).toBe(2);
-
     expect(screen.getByText("Cart is empty Please add items!!")).toBeInTheDocument();
 
 })
